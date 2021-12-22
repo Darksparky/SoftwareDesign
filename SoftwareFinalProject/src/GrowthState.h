74f29633f7662a90d6ec61c8e3b559eed52b4eb9
Christopher Clarke <dark_sparky@hotmail.com>
2021-12-22 15:43:12 -0500

    Update GrowthState.h

--- SoftwareFinalProject/src/GrowthState.h
+++ SoftwareFinalProject/src/GrowthState.h
@@ -1,1 +1,22 @@
 #pragma once
+
+
+class GrowthState {
+private:
+	int timeElapsed;
+	int stateDuration;
+	bool canBeHarvested;
+	ofImage stateImage;
+	string Name;
+
+public:
+	GrowthState(int duration, string n, ofImage i);
+	bool GetCanBeHarvested();
+	int GetTimeElapsed();
+	int GetStateDuration();
+
+};
+
+class CropsManager {
+
+};
